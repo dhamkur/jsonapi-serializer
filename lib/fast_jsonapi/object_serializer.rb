c7e2bb864090016c141401e0a7225aa734d960e5
Adham Kurniawan <adhamkurniawan29@gmail.com>
2021-11-10 03:27:50 +0700

    fixed simplified options

--- lib/fast_jsonapi/object_serializer.rb
+++ lib/fast_jsonapi/object_serializer.rb
@@ -15,7 +15,6 @@ module FastJsonapi
     extend ActiveSupport::Concern
     include SerializationCore
 
-    SIMPLE_ATTRIBUTES = "false"
     TRANSFORMS_MAPPING = {
       camel: :camelize,
       camel_lower: [:camelize, :lower],
@@ -28,9 +27,10 @@ module FastJsonapi
       set_type(reflected_record_type) if reflected_record_type
     end
 
-    def initialize(resource, options = {})
+    def initialize(resource, simplified = false, options = {})
       process_options(options)
 
+      @simplified = ENV["SIMPLIFIED"].present? ? ENV["SIMPLIFIED"] : simplified
       @resource = resource
     end
 
@@ -53,7 +53,7 @@ module FastJsonapi
       serializable_hash[:data] = self.class.record_hash(@resource, @fieldsets[self.class.record_type.to_sym], @includes, @params)
       serializable_hash[:included] = self.class.get_included_records(@resource, @includes, @known_included_objects, @fieldsets, @params) if @includes.present?
 
-      if SIMPLE_ATTRIBUTES == "true"
+      if @simplified?
         simple_attributes = {
           data: serializable_hash[:data][:attributes],
           relationships: serializable_hash[:included]
@@ -74,7 +74,7 @@ module FastJsonapi
         included.concat self.class.get_included_records(record, @includes, @known_included_objects, @fieldsets, @params) if @includes.present?
       end
 
-      if SIMPLE_ATTRIBUTES == "true"
+      if @@simplified?
         serializable_hash[:data] = data
         serializable_hash[:included] = included if @includes.present?
         serializable_hash[:meta] = @meta if @meta.present?
